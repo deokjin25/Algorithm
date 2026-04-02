e76f66093a909bdf8b19a33f7e07e6b4591885e6
deokjin <ghaehfdl7@naver.com>
2026-04-02 22:34:11 +0900

    0402: 가장 긴 증가하는 부분 수열

---
+++ Python/BJ/DP/BJ_11053.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+from bisect import bisect_left
+
+n = int(input())
+seq = list(map(int, input().split()))
+lis = []
+for x in seq :
+  idx = bisect_left(lis, x)
+  if idx == len(lis) :
+    lis.append(x)
+  else :
+    lis[idx] = x
+
+print(len(lis))

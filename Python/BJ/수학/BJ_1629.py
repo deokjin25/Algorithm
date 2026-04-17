a0f700fb6005f01bf5ebba1c178b6a0981504a6a
deokjin <ghaehfdl7@naver.com>
2026-04-17 23:31:17 +0900

    0417: 곱셈

--- Python/BJ/수학/BJ_1629.py
+++ Python/BJ/수학/BJ_1629.py
@@ -1,16 +1,29 @@
 a, b, c = map(int, input().split())
 
-dp = {}
+# dp = {}
 
-def split_mul(x) :
-  if x in dp:
-    return dp[x]
+# def split_mul(x) :
+#   if x in dp:
+#     return dp[x]
   
-  if x == 1 :
-    return a%c
+#   if x == 1 :
+#     return a%c
   
-  dp[x] = split_mul(x//2) * split_mul(x - x//2) % c
+#   dp[x] = split_mul(x//2) * split_mul(x - x//2) % c
 
-  return dp[x]
+#   return dp[x]
 
-print(split_mul(b)%c)
+# print(split_mul(b))
+
+# print(pow(a,b,c))
+
+def split_mul(x):
+    if x == 1:
+        return a % c
+    half = split_mul(x // 2)
+    if x % 2 == 0:
+        return half * half % c
+    else:
+        return a % c * half * half % c  # 홀수면 a 한 번 더 곱
+    
+print(split_mul(b))
